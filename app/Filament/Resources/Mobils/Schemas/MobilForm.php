52a6103b2f00cf302d88fe6c08963ed237d86b00
Juan <juanawbest@gmail.com>
2025-10-01 15:40:49 +0800

    Form Mobil --

--- app/Filament/Resources/Mobils/Schemas/MobilForm.php
+++ app/Filament/Resources/Mobils/Schemas/MobilForm.php
@@ -26,26 +26,34 @@ class MobilForm
                             ->placeholder('Contoh: DA 1234 XX')
                             ->prefixIcon('heroicon-o-identification')
                             ->required(),
-                            
+
                         Select::make('nama_tim')
-                            ->label('Tim Armada')
-                            ->placeholder('Pilih Tim')
-                            ->searchable()
-                            ->prefixIcon('heroicon-o-users')
-                            ->required()
-                            ->createOptionForm([
-                                    TextInput::make('nama_tim')
-                                        ->label('Tim Armada Baru')
-                                        ->required(),
-                                ])
-                                ->createOptionAction(fn(Action $action) => $action
-                                    ->modalHeading('Tambah Nomor Unit')
-                                    ->modalSubmitActionLabel('Simpan')
-                                    ->modalWidth('md'))
-                                ->createOptionUsing(fn(array $data) => $data['nama_tim']),
-                            
+    ->label('Tim Armada')
+    ->placeholder('Pilih Tim')
+    ->searchable()
+    ->prefixIcon('heroicon-o-users')
+    ->required()
+    ->options(fn () => Mobil::distinct()->pluck('nama_tim', 'nama_tim')->toArray())
+    ->createOptionForm([
+        TextInput::make('nama_tim')
+            ->label('Tim Armada Baru')
+            ->required(),
+    ])
+    ->createOptionAction(fn(Action $action) => $action
+        ->modalHeading('Tambah Tim Armada')
+        ->modalSubmitActionLabel('Simpan')
+        ->modalWidth('md'))
+    ->createOptionUsing(function (array $data) {
+        // Bikin dummy Mobil hanya untuk nyimpen nama_tim baru
+        Mobil::firstOrCreate(['nama_tim' => $data['nama_tim']]);
+
+        // balikin string supaya langsung jadi selected
+        return $data['nama_tim'];
+    }),
+
                         Select::make('no_unit')
-                            ->options(Mobil::distinct()->pluck('no_unit', 'no_unit'))
+                                ->label('Nomor Unit Mobil')
+                                ->options(Mobil::distinct()->pluck('no_unit', 'no_unit'))
                                 ->searchable()
                                 ->placeholder('Pilih atau Tambahkan No. Unit')
                                 ->prefixIcon('heroicon-o-hashtag')
@@ -64,19 +72,19 @@ class MobilForm
                         ToggleButtons::make('status_mobil')
                             ->options(['Aktif' => 'Aktif', 'Tidak Aktif' => 'Tidak aktif', 'Dalam Perbaikan' => 'Dalam perbaikan'])
                             ->icons([
-                                    'Aktif' => 'heroicon-o-check-circle',
-                                    'Tidak Aktif' => 'heroicon-o-x-circle',
-                                    'Dalam Perbaikan' => 'heroicon-o-wrench-screwdriver',
-                                ])
+                                'Aktif' => 'heroicon-o-check-circle',
+                                'Tidak Aktif' => 'heroicon-o-x-circle',
+                                'Dalam Perbaikan' => 'heroicon-o-wrench-screwdriver',
+                            ])
                             ->colors([
-                                    'Aktif' => 'success',
-                                    'Tidak Aktif' => 'warning',
-                                    'Dalam Perbaikan' => 'danger',
-                                ])
+                                'Aktif' => 'success',
+                                'Tidak Aktif' => 'warning',
+                                'Dalam Perbaikan' => 'danger',
+                            ])
                             ->inline(true)
                             ->grouped()
                             ->required(),
-                            
+
                         Textarea::make('merk_mobil')
                             ->label('Informasi Detail Mobil')
                             ->placeholder('Contoh: Toyota Avanza, Daihatsu Xenia, dll.')
--- app/Models/Mobil.php
+++ app/Models/Mobil.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Mobil extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nomor_plat',
+        'merk_mobil',
+        'no_unit',
+        'nama_tim',
+        'status_mobil',
+    ];
 }
